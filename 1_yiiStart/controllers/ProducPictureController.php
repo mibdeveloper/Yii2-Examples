ea65609e1faaf6b08906f96b467b37e54661094f
Viacheslav <mibdeveloper@gmail.com>
2015-10-15 20:46:25 +0300

    categories list in product

---
+++ 1_yiiStart/controllers/ProducPictureController.php
@@ -0,0 +1,12 @@
+?php
+
+namespace app\controllers;
+
+class ProducPictureController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
---
+++ 1_yiiStart/views/product/_form.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Product */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?/*= $form->field($model, 'categoryid')->textInput()*/ ?>
+
+    <?= $form->field( $model, 'categoryid')
+            ->dropDownList( $model->categoriesList, ['prompt'=>'Select...'])?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'incoming_price')->textInput() ?>
+
+    <?= $form->field($model, 'price')->textInput() ?>
+
+    <?= $form->field($model, 'article')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'date')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
